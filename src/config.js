a5ba6963090d9c92f9b109c3ef0e34409e339f0d
Daniel Feng <daniel.feng@metropia.com>
2024-04-27 14:52:50 +0800

    feat: add env file

--- src/config.js
+++ src/config.js
@@ -1,13 +1,16 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
 const config = {
-    port: 3000,
-    database: {
-        user: 'postgres',
-        host: 'localhost',
-        password: 'postgres',
-        port: 5432,
-    },
-    JWT_SECRET_KEY: "~!@#$%`12345",
-    PBKDF2_SALT: "~!@#$%0123456789"
-  }
-  
-  export default config;
+  port: 3000,
+  database: {
+    user: 'postgres',
+    host: 'localhost',
+    password: 'postgres',
+    port: 5432,
+  },
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  PBKDF2_SALT: process.env.PBKDF2_SALT,
+};
+
+export default config;
